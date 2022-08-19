1545ba5fa948ac97a0d5c0cbe2030dcb0684d9b1
CreyValentine <valentine.crey95@gmail.com>
2022-08-19 06:22:49 -0500

    defeated

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,32 @@
+// create function for current day
+var todaysDate = new Date()
+//try to create a variable to stringify and set time in local storage
+//create another variable to get time from local storage?
+// how to use today variable to display day after set and get 
 var today = document.querySelector("#currentDay");
-today = function Date() {
-    var n = today.toDateString();
-    var time = today.toLocalTimeString();
+today = new Date();
 
+//window.localStorage.setItem("date", JSON.stringify(date));
+
+
+
+/*
+var currentDay = {
+    day:"today",
+    month:"month",
+    date:"present",
+}
+*/
+
+//window.localStorage.setItem("date", JSON.stringify(date));
+
+
+
+var today = document.querySelector("#currentDay");
+today = function() {
+    var todaysDate = new Date(); //this will not work
+    todaysDate.getDay(); // neither will this. json wont read function 
 };
 
-Date();
+console.log(today);
 
