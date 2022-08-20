f06309dd01945b8d649ad6606a90977a3d80ac36
CreyValentine <valentine.crey95@gmail.com>
2022-08-20 07:30:54 -0500

    pseudocode for button event

--- Develop/script.js
+++ Develop/script.js
@@ -5,11 +5,7 @@ var todaysDate = new Date()
 // how to use today variable to display day after set and get 
 var today = document.querySelector("#currentDay");
 today = new Date();
-
 //window.localStorage.setItem("date", JSON.stringify(date));
-
-
-
 /*
 var currentDay = {
     day:"today",
@@ -17,11 +13,7 @@ var currentDay = {
     date:"present",
 }
 */
-
 //window.localStorage.setItem("date", JSON.stringify(date));
-
-
-
 var today = document.querySelector("#currentDay");
 today = function() {
     var todaysDate = new Date(); //this will not work
@@ -29,4 +21,14 @@ today = function() {
 };
 
 console.log(today);
+
+//create button for rows dynamically.
+//button class="btn"
+//use appendChild to assign button to .col-sm-2 (furthest to right)
+//create array with variable name eventInput (empty array?) to store event input
+//JSON stringify inputs and use windows.localStorage.setItem()
+//use addEventListener for save button click event 
+//when button is clicked I want the event to stay displayed on event column
+//create eventHandler function with addEventListener in it
+//create function expression with name userInput. (use eventHandler as parameter?) use if statement with array, localStorage.getItem(), and return() to store on event column
 
