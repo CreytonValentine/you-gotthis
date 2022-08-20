1243696fdb6f3a30b640b33957cc120790fe5132
CreyValentine <valentine.crey95@gmail.com>
2022-08-20 07:54:26 -0500

    added id to button column

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
 // create function for current day
-var todaysDate = new Date()
+//var todaysDate = new Date()
 //try to create a variable to stringify and set time in local storage
 //create another variable to get time from local storage?
 // how to use today variable to display day after set and get 
-var today = document.querySelector("#currentDay");
-today = new Date();
+//var today = document.querySelector("#currentDay");
+//today = new Date();
 //window.localStorage.setItem("date", JSON.stringify(date));
 /*
 var currentDay = {
@@ -14,13 +14,15 @@ var currentDay = {
 }
 */
 //window.localStorage.setItem("date", JSON.stringify(date));
-var today = document.querySelector("#currentDay");
+//var today = document.querySelector("#currentDay");
+/*
 today = function() {
     var todaysDate = new Date(); //this will not work
     todaysDate.getDay(); // neither will this. json wont read function 
 };
+*/
 
-console.log(today);
+//console.log(today);
 
 //create button for rows dynamically.
 //button class="btn"
@@ -31,4 +33,12 @@ console.log(today);
 //when button is clicked I want the event to stay displayed on event column
 //create eventHandler function with addEventListener in it
 //create function expression with name userInput. (use eventHandler as parameter?) use if statement with array, localStorage.getItem(), and return() to store on event column
+var buttonColumn = "#btn-column";
+var btn = document.createElement("button");
+btn.textContent = "save";
+
+buttonColumn.appendChild(btn);
+
+
+
 
